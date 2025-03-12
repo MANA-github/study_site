396930c6d33e42568e0a0a0cb69dc6a72cd6c0fe
MANA-github <153607046+MANA-github@users.noreply.github.com>
2025-03-13 00:32:02 +0900

    a

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,22 +1,19 @@
-async function getMenuJson() {
-    const response = await fetch("https://test.manawork79.workers.dev/api/menu");
-    const data = await response.json();
+const response = await fetch("https://test.manawork79.workers.dev/api/menu");
+const data = await response.json();
 
-    console.log(data);
-    document.getElementById("response").innerText = "Check the console log !";
+console.log(data);
 
-    const gridContainer = document.querySelector(".grid-container");
+const gridContainer = document.querySelector(".grid-container");
     
-    gridContainer.innerHTML = "";
+gridContainer.innerHTML = "";
 
-    data.forEach(item => {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");    
-        gridItem.innerHTML = `
-            <img src="${item.image_url}" alt="${item.name}" style="width: 100px; height: 100px;">
-            <p>${item.name}</p>
-            <p>${item.price}円</p>
-        `;
-        gridContainer.appendChild(gridItem);
-    });
-}
+data.forEach(item => {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");    
+    gridItem.innerHTML = `
+        <img src="${item.image_url}" alt="${item.name}" style="width: 100px; height: 100px;">
+        <p>${item.name}</p>
+        <p>${item.price}円</p>
+    `;
+    gridContainer.appendChild(gridItem);
+});
