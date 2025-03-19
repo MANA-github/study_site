a07fbec1594f972bf98a54b3693e15b779fc0907
MANA-github <153607046+MANA-github@users.noreply.github.com>
2025-03-20 02:01:28 +0900

    d#	modified:   public/purchase.html

--- assets/js/purchase.js
+++ assets/js/purchase.js
@@ -9,6 +9,26 @@ async function selectMenu() {
     }
 
     console.log(MenuId);
+
+    const response = await fetch(`https://database.manawork79.workers.dev/api/menu/?id=${MenuId}`);
+    const data = await response.json();
+
+    const item = document.querySelector(".menu");
+
+     if (!data.Available) {
+        const text = "提供不可";
+     }
+     else {
+        const text = `${data.price}円`;
+     }
+
+    contents.innerHTML = `
+        <img src="${data.ImgUrl}" alt="${data.MenuName}" style="width: 100px; height: 100px;">
+        <p>${data.MenuName}</p>
+        <p>残り${data.Remaining}個　${text}</p>
+    `;
+
+    item.appendChild(contents);
 }
 
 window.addEventListener("DOMContentLoaded", selectMenu);
